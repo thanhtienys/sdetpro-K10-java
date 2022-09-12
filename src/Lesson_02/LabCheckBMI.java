a61b6084ba3b8df296035ab92b7941b239cc4045
thanhtienys <thanh.tien.ys@gmail.com>
2022-09-12 10:29:20 +0700

    lab 2.3

--- src/Lesson_02/LabCheckBMI.java
+++ src/Lesson_02/LabCheckBMI.java
@@ -22,18 +22,30 @@ public class LabCheckBMI {
 
         //Show BMI
         System.out.printf("BMI of you is %f:", BMI);
-        System.out.println("\n");
+        System.out.print("\n");
+
+        // Calculate the weight to increase
+        float BMI_NORMAL_MIN = 18.6f;
+        float weightNormalMin = (BMI_NORMAL_MIN * height * 2);
+        float weightIncrease =(weightNormalMin - weight);
+
+        // Calculate the weight to decrease
+        float BMI_NORMAL_MAX = 24.9f;
+        float weightNormalMax = (BMI_NORMAL_MAX * height * 2);
+        float weightDecrease =(weight - weightNormalMax);
 
         //Classify of BMI
-
         if (BMI <= 18.5){
             System.out.println("You are underweight");
+            System.out.printf("You need increase: %f kg" ,+ weightIncrease , "kg");
         }else if (BMI <= 24.9) {
             System.out.println(" You are normal weight");
         }else if (BMI <= 29.9) {
             System.out.println("You are overweight");
+            System.out.printf("You need decrease: %f kg" ,+ weightDecrease , "kg");
         }else {
             System.out.println("You are obesity");
+            System.out.printf("You need decrease: %f kg" ,+ weightDecrease , "kg");
         }
     }
 }
