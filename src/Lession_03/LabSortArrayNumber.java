f24b7932bcce04922d235bccf1c293a99e065463
Thanh Tiền <thanh.tien.ys@gmail.com>
2022-09-17 12:43:08 +0700

    Lab 3.3

---
+++ src/Lession_03/LabSortArrayNumber.java
@@ -0,0 +1,45 @@
+package src.Lession_03;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class LabSortArrayNumber {
+
+    public static void main(String[] args) {
+
+        //Input length array
+        int length;
+        do{
+            System.out.print("Input length of array:");
+            Scanner scanner = new Scanner(System.in);
+            length = scanner.nextInt();
+        }while (length <= 0);
+
+        //Create array number random
+        System.out.println("My array number random is:");
+        int [] arrNum = new int[length];
+        for (int i = 0; i < length; i++) {
+            int randomNumber = new SecureRandom().nextInt(1000);
+            arrNum[i] = randomNumber;
+            System.out.printf("  %d",arrNum[i]);
+        }
+
+        //Sort array number
+        int varTemporary;
+        for (int i = 0; i < (length - 1); i++) {
+            for (int j = (i+1) ; j < length; j++) {
+                if (arrNum[i] > arrNum[j]) {
+                    varTemporary = arrNum[i];
+                    arrNum[i] = arrNum[j];
+                    arrNum[j] = varTemporary;
+                }
+            }
+
+        }
+        //Display array number after sorted
+        System.out.println("\n My array after sorted:");
+        for (int i = 0; i < length; i++) {
+            System.out.printf("  %d",arrNum[i]);
+        }
+    }
+}
