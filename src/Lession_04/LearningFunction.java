8359d1290dec962b515f906d7437831c548df466
thanhtienys <thanh.tien.ys@gmail.com>
2022-09-19 09:14:25 +0700

    Learning Function

---
+++ src/Lession_04/LearningFunction.java
@@ -0,0 +1,35 @@
+package src.Lession_04;
+
+import java.security.SecureRandom;
+
+public class LearningFunction {
+
+    public static void main(String[] args) {
+        printARandomNumber();
+    }
+
+    // Access Modifier | class method | returned type
+    public static void printARandomNumber(){
+
+        System.out.println(new SecureRandom().nextInt(1000));
+    }
+}
+
+/*
+-> Goi function co truyen paramater
+    public static void main(String[] args) {
+        // 2 | Agrument;
+        printARandomNumber(2);
+    }
+    // inputNumber | Parameter
+    public static void printARandomNumber (int inputNumber){
+        System.out.println(inputNumber);
+    }
+    // truyen nhieu Parameter
+    // inputNumber | Parameter
+    public static void printARandomNumber (int inputNumber , int seconNumber){
+        System.out.println(inputNumber);
+    }
+
+ */
+
